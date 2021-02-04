c8c8092d1df2bf40fd5a3f9df33fafd3f5789168
Rouxie <mpraida@hotmail.com>
2021-02-04 16:55:48 -0600

    v.1

---
+++ js/tabs.js
@@ -0,0 +1,53 @@
+//objeto con propiedades tab
+var propTabs={
+	primer_encabezado: document.getElementById('encabezado_menu').firstElementChild,
+	primer_contenido: document.getElementById('contenido_menu').firstElementChild,
+	enlaces_encabezado: document.querySelectorAll('#encabezado_menu li a'),
+	li_encabezado: document.querySelectorAll('#encabezado_menu li'),
+	divs_contenido: document.querySelectorAll('#contenido_menu > div'),
+	contenido_activo: null
+
+}
+
+
+//objeto con metodos de tab 
+
+var metTabs={
+
+	inicio: function () {
+		propTabs.primer_encabezado.className = "active";
+		propTabs.primer_contenido.className = "active";
+
+		for (var i = 0; i < propTabs.enlaces_encabezado.length;i++) {
+			propTabs.enlaces_encabezado[i].addEventListener("click", metTabs.evento);
+		}
+	},
+
+	evento: function(e){
+		e.preventDefault();
+
+		for (var i =0; i < propTabs.li_encabezado.length; i++) {
+			propTabs.li_encabezado[i].className = " ";
+		}
+
+		for (var i =0; i < propTabs.divs_contenido.length; i++) {
+			propTabs.divs_contenido[i].className = " ";
+		}
+
+		this.parentElement.className = "active";
+
+		propTabs.contenido_activo = this.getAttribute("href");
+		document.querySelector(propTabs.contenido_activo).style.opacity = 0;
+
+		document.querySelector(propTabs.contenido_activo).className = "active";
+		setTimeout(function(){
+			document.querySelector(propTabs.contenido_activo).style.opacity = 1;
+		}, 100);
+
+
+
+
+	}
+
+}
+metTabs.inicio();
